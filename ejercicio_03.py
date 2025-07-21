79c262bc39c1746faef884b619eb0d5e51f3626d
Maykmora <maykmora12@gmail.com>
2025-07-21 16:29:13 -0600

    commit 4 ejercicio 3

--- ejercicio_03.py
+++ ejercicio_03.py
@@ -34,4 +34,20 @@ else:
             print("2.Cambiar contraseña")
             print("3.Cerrar sesión")
 
+        else:
+            print("Usuario y contraseña incorrecta, ultimo intento")
+            print()
+            user = input("Ingrese su usuario:")
+            password = input("Ingrese su contraseña:")
+
+            if user in users and users[user] == password:
+                print("Usuario y contraseña correcta, bienvenido al menu")
+                print("1.Ver perfil")
+                print("2.Cambiar contraseña")
+                print("3.Cerrar sesión")
+
+            else:
+                print("Ya no te quedan intentos")
+                print("Acceso bloqueado")
+
 
