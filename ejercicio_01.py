ba45bb5e600e7738b6ef004c0dcc232715a50070
Maykmora <maykmora12@gmail.com>
2025-07-21 15:07:28 -0600

    commit 1 ejercicio 1

---
+++ ejercicio_01.py
@@ -0,0 +1,7 @@
+print("Bienvenido al programa")
+name=input("Ingrese su nombre:").lower()
+dpi=int(input("Ingrese su DPI:"))
+department=input("Ingrese su departamento de origen:")
+year=int(input("Ingrese su año de nacimiento: "))
+
+print(name)
