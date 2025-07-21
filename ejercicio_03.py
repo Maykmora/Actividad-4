195c052b38925d7a87995b14125a27592b03b006
Maykmora <maykmora12@gmail.com>
2025-07-21 16:24:53 -0600

    commit 3 ejercicio 3- casi un bucle

--- ejercicio_03.py
+++ ejercicio_03.py
@@ -3,3 +3,35 @@ users={"maynor":"mouse25", "juanito":"admin123", "carlos":"windows23"}
 
 user=input("Ingrese su usuario:")
 password=input("Ingrese su contraseña:")
+
+if user in users and users[user]==password:
+    print("Usuario y contraseña correcta, bienvenido al menu")
+    print("1.Ver perfil")
+    print("2.Cambiar contraseña")
+    print("3.Cerrar sesión")
+
+else:
+    print("Usuario y contraseña incorrecta, intento 1 de 3")
+    print()
+    user = input("Ingrese su usuario:")
+    password = input("Ingrese su contraseña:")
+
+    if user in users and users[user] == password:
+        print("Usuario y contraseña correcta, bienvenido al menu")
+        print("1.Ver perfil")
+        print("2.Cambiar contraseña")
+        print("3.Cerrar sesión")
+
+    else:
+        print("Usuario y contraseña incorrecta, intento 2 de 3")
+        print()
+        user = input("Ingrese su usuario:")
+        password = input("Ingrese su contraseña:")
+
+        if user in users and users[user] == password:
+            print("Usuario y contraseña correcta, bienvenido al menu")
+            print("1.Ver perfil")
+            print("2.Cambiar contraseña")
+            print("3.Cerrar sesión")
+
+
