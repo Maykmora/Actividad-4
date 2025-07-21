5020570776e748a9cbcbc269d20bbb0066b2d382
Maykmora <maykmora12@gmail.com>
2025-07-21 17:42:41 -0600

    commit 2 ejercicio 5

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -1,8 +1,9 @@
 print("Bienvenido al programa")
 day=int(input("Ingrese el numero de dia que desea:"))
 month=int(input("Ingrese el numero de mes que desea:"))
-year=int(input("Ingrese el año que desee"))
+year=int(input("Ingrese el año que sea hasta de 4 digitos"))
 
-if 0<day<=31 and 0<month<=12 and year>0:
+if 0<day<=31 and 0<month<=12 and 0<year>9999:
     print("Fecha valida")
-
+    k=year%10
+    print(k)
