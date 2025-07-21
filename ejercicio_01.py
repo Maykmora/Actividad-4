6a7c94453ae3e4750a2e454f32071114020a39cf
Maykmora <maykmora12@gmail.com>
2025-07-21 15:16:04 -0600

    commit 2 ejercicio 1

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -1,7 +1,12 @@
 print("Bienvenido al programa")
+print()
 name=input("Ingrese su nombre:").lower()
 dpi=int(input("Ingrese su DPI:"))
-department=input("Ingrese su departamento de origen:")
+department=input("Ingrese su departamento de origen:").lower()
 year=int(input("Ingrese su año de nacimiento: "))
 
-print(name)
+if year>2007:
+    print("Puede votar")
+
+if department == "petén" or department== "alta verapaz" and year==2008:
+    print("Puede votar")
