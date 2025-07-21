39e6753895c10d3d73a705e7bd31732ad96882c2
Maykmora <maykmora12@gmail.com>
2025-07-21 16:00:03 -0600

    commit 1 ejercicio 2

---
+++ ejercicio_02.py
@@ -0,0 +1,7 @@
+print("Bienvenido al programa")
+
+salary=int(input("Ingrese su salario anual:"))
+dependents=int(input("Ingrese el numero de dependientes:"))
+
+if salary>0 and salary<30000:
+    print()
