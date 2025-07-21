c9bd522a4e8bcd528d8a5ba0fc7a82f5f343adb5
Maykmora <maykmora12@gmail.com>
2025-07-21 15:25:52 -0600

    commit 3 ejercicio 1

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -6,7 +6,11 @@ department=input("Ingrese su departamento de origen:").lower()
 year=int(input("Ingrese su año de nacimiento: "))
 
 if year>2007:
-    print("Puede votar")
+    print("No puedes votar porque eres menor de edad")
 
-if department == "petén" or department== "alta verapaz" and year==2008:
-    print("Puede votar")
+elif department == "petén" or department== "alta verapaz" and year<2008:
+    print(f"Puede votar porque es de {department} y tiene 17 años o menos")
+    print(f"Bienvenido {name}, su centro de votación esta en {department}")
+
+elif len(name)<5 or len(str(dpi))<13 or len(str(dpi))>13:
+    print("No puedes votar porque tu nombre tiene menos de 5 letras o no ingresaste correctamente el dpi")
