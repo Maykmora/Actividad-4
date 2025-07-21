db97c97afc4e34814bf7f4a26b5d2bd24d0cb7db
Maykmora <maykmora12@gmail.com>
2025-07-21 16:20:48 -0600

    commit 2 ejercicio 3

--- ejercicio_03.py
+++ ejercicio_03.py
@@ -1,2 +1,5 @@
 print("Bienvenido al programa")
-users={}
+users={"maynor":"mouse25", "juanito":"admin123", "carlos":"windows23"}
+
+user=input("Ingrese su usuario:")
+password=input("Ingrese su contraseña:")
