49eb8902f10848033bdf454115b6573f9a988199
Maykmora <maykmora12@gmail.com>
2025-07-21 18:24:35 -0600

    commit 1 ejercicio 6

---
+++ ejercicio_06.py
@@ -0,0 +1,4 @@
+weight=int(input("Ingrese el peso del paquete en kg:"))
+distance=int(input("Ingrese la distancia en km:"))
+urgency=input("¿Es de urgencia? (si/no):")
+size=input("Ingrese el tamaño del paquete (pequeño/mediano/grande)")
