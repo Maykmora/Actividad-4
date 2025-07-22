36e459bd745cecccf2c4a3400fee368a796100c6
Maykmora <maykmora12@gmail.com>
2025-07-21 18:42:53 -0600

    commit 3 ejercicio 6

---
+++ ejercicio_07.py
@@ -0,0 +1,12 @@
+print("Bienvenido al programa")
+students=[]
+
+for i in range(5):
+    nombre=input(f"Nombre del estudiante {i+1}:")
+    notas=[]
+    for j in range(3):
+        nota=float(input(f"Nota {j+1} de {nombre}:"))
+        notas.append(nota)
+    students.append({"nombre": nombre, "notas": notas})
+
+print(students)
