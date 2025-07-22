b0a125e0a8254ac1129602c9da06c7a63e41c86a
Maykmora <maykmora12@gmail.com>
2025-07-21 19:22:35 -0600

    commit 2 ejercicio 9

--- ejercicio_09.py
+++ ejercicio_09.py
@@ -5,3 +5,28 @@ student=input("¡Es estudiante?:")
 
 if age<13:
     print("No puedes ver peliculas para mayores de 15 ")
+    if day=="miercoles":
+        print("El día miercoles hay 2x1")
+        if student=="si":
+            print("Por ser estudiante paga Q.35")
+        else:
+            print("Por no ser estudiante paga Q.50")
+    else:
+        if student=="si":
+            print("Por ser estudiante paga Q.35")
+        else:
+            print("Por no ser estudiante paga Q.50")
+
+
+else:
+    if day=="miercoles":
+        print("El día miercoles hay 2x1")
+        if student=="si":
+            print("Por ser estudiante paga Q.35")
+        else:
+            print("Por no ser estudiante paga Q.50")
+    else:
+        if student=="si":
+            print("Por ser estudiante paga Q.35")
+        else:
+            print("Por no ser estudiante paga Q.50")
