44d215fa19b56af45d82d8366e139c2c7ddef1a0
Maykmora <maykmora12@gmail.com>
2025-07-21 18:19:11 -0600

    commit 4 ejercicio 5

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -3,9 +3,16 @@ day=int(input("Ingrese el numero de dia que desea:"))
 month=int(input("Ingrese el numero de mes que desea:"))
 year=int(input("Ingrese el año que sea hasta de 4 digitos:"))
 
+dias=["Sabado","Domingo", "Lunes", "Martes", "Miercoles", "Jueves","Viernes"]
+
 if 0<day<=31 and 0<month<=12 and 0<year<=9999:
     print("Fecha valida")
-    k=year//100
-    j=year%100
-    print(k)
-    print(j)
+    j=year//100
+    k=year%100
+
+    h=int((day+(2.6*(month+1))+k+(k/4)+(j/4)-2*j)%7)
+    print("El dia que seleccionaste fue", dias[h])
+
+else:
+    print("Fecha invalida intentelo de nuevo.")
+
