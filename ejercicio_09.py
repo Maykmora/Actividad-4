5ca671c1af8c71424146f240a6b3d6ec811cedf6
Maykmora <maykmora12@gmail.com>
2025-07-21 19:13:03 -0600

    commit 1 ejercicio 9

---
+++ ejercicio_09.py
@@ -0,0 +1,7 @@
+print("Bienvenido al programa")
+age=int(input("Ingrese su edad:"))
+day=input("Ingrese el dia de la semana que desea:")
+student=input("¡Es estudiante?:")
+
+if age<13:
+    print("No puedes ver peliculas para mayores de 15 ")
