2a425872a4e2fd7f4c4c0de344435e32c5ec8bb1
Maykmora <maykmora12@gmail.com>
2025-07-21 20:22:26 -0600

    commit 3 ejercicio 10

--- ejercicio_10.py
+++ ejercicio_10.py
@@ -14,19 +14,19 @@ if date1>date2:
     print("La primer fecha es mayor")
     if m1 == m2 and a1 == a2:
         print("Las fechas están en el mismo mes y año.")
-        print(f"Han pasado:{d1 - d2} días,{m1 - m2} meses ,{a1 - a3} años")
+        print(f"Han pasado:{abs(d1 - d2)} días,{abs(m1 - m2)} meses ,{abs(a1 - a2)} años")
     else:
         print("Las fechas NO están en el mismo mes y año.")
-        print(f"Han pasado:{d1 - d2} días,{m1 - m2} meses ,{a1 - a3} años")
+        print(f"Han pasado:{abs(d1 - d2)} días,{abs(m1 - m2)} meses ,{abs(a1 - a2)} años")
 
 elif date1<date2:
     print("La segunda fecha es mayor.")
     if m1 == m2 and a1 == a2:
         print("Las fechas están en el mismo mes y año.")
-        print(f"Han pasado:{d1 - d2} días,{m1 - m2} meses ,{a1 - a3} años")
+        print(f"Han pasado:{abs(d1 - d2)} días,{abs(m1 - m2)} meses ,{abs(a1 - a2)} años")
     else:
         print("Las fechas NO están en el mismo mes y año.")
-        print(f"Han pasado:{abs(d1 - d2)} días,{(m1 - m2)} meses ,{(a1 - a2)} años")
+        print(f"Han pasado:{abs(d1 - d2)} días,{abs(m1 - m2)} meses ,{abs(a1 - a2)} años")
 
 else:
     print("Ambas fechas son iguales.")
